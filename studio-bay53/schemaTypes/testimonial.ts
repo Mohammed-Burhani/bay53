5cd3396c32216e1087033081f272a55d1ca44ac1
Mohammed <heavilgaming72@gmail.com>
2026-02-02 17:26:00 +0530

    Fixed testimonial revalidation

--- studio-bay53/schemaTypes/testimonial.ts
+++ studio-bay53/schemaTypes/testimonial.ts
@@ -6,12 +6,6 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-      validation: (Rule) => Rule.required(),
-    }),
-    defineField({
       name: 'role',
       title: 'Role',
       type: 'string',
